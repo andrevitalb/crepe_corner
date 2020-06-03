e1dfe6f965032c6175107d034460b92c95e5afe9
André Vital <santiago_vital@outlook.com>
2020-06-03 17:03:58 -0500

    Added php for email sending

---
+++ franq_form.php
@@ -0,0 +1,149 @@
+<meta http-equiv="refresh" content="2.5;URL=franquicias.html"/>
+<?php
+    date_default_timezone_set('America/Mexico_City');
+
+    $name = $_REQUEST['franqName'];
+    $email = $_REQUEST['franqEmail'];
+    $tel = $_REQUEST['franqTel'];
+    $nat = $_REQUEST['franqNat'];
+    $ocup = $_REQUEST['franqOcup'];
+    $age = $_REQUEST['franqAge'];
+
+
+    $ownBusiness = $_REQUEST['franqOwnBusiness'];
+    if($ownBusiness == "yes") $ownBusiness = "Sí";
+    else $ownBusiness = "No";
+
+    $prevBusiness = $_REQUEST['franqPrevBusiness'];
+
+    $gastExperience = $_REQUEST['franqGastExperience'];
+    if($gastExperience == "yes") $gastExperience = "Sí";
+    else $gastExperience = "No";
+
+    $franqSystem = $_REQUEST['franqFranqSystem'];
+    if($franqSystem == "yes") $franqSystem = "Sí";
+    else $franqSystem = "No";
+
+    $operate = $_REQUEST['franqOperate'];
+
+    
+    $invertAmount = $_REQUEST['franqInvertAmount'];
+    if($invertAmount == "40-60") $invertAmount = "US $40'000 - $60'000";
+    else if($invertAmount == "60-100") $invertAmount = "US $60'000 - $100'000";
+    else $invertAmount = "US $100'000 - $150'000";
+
+    $invertRecover = $_REQUEST['franqInvertRecover'];
+    if($invertRecover == "12") $invertRecover = "12 meses";
+    else $invertRecover = "18 meses";
+
+    $location = $_REQUEST['franqLocation'];
+
+    $ownPlace = $_REQUEST['franqOwnPlace'];
+    if($ownPlace == "yes") $ownPlace = "Sí";
+    else $ownPlace = "No";
+
+    $start = $_REQUEST['franqStart'];
+
+    $headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+	$headers .= 'From: '.$name.' <'.$email.'>'."\r\n";
+
+	$message = "
+	<html>
+	<head>
+	<title>Formulario Franquicias</title>
+	</head>
+	<body>
+	<table width='50%' border='0' align='center' cellpadding='0' cellspacing='0'>
+	<tr>
+	<td colspan='2' align='center' valign='top'><img style=' width: 175px; margin-top: 15px; ' src='https://www.yellowpath.mx/proyectos/crepe-corner/assets/images/logo/logo-crepe_corner-color.png'></td>
+	</tr>
+	<tr>
+	<td width='50%' align='right'>&nbsp;</td>
+	<td align='left'>&nbsp;</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Nombre:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$name</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Correo:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$email</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Teléfono:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$tel</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Nacionalidad:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$nat</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Ocupación:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$ocup</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Edad:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$age</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Negocio propio:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$ownBusiness</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Tipo de negocio:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$prevBusiness</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Experiencia en gastronomía:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$gastExperience</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Manejo de sistema de franquicia:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$franqSystem</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Operador de franquicia:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$operate</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Nivel de negocio:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$invertAmount</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Tiempo esperado de recuperación:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$invertRecover</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Ubicación planeada de comercio:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$location</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Local propio:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$ownPlace</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Inicio deseado:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>$start</td>
+	</tr>
+	<tr>
+	<td colspan='2' align='center' valign='top' style='font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Enviado el:</strong> ".date('d-m-Y H:i:s')."</td>
+	</tr>
+	<tr>
+	<td width='50%' align='right'>&nbsp;</td>
+	<td align='left'>&nbsp;</td>
+	</tr>
+	<tr>
+	<td width='50%' align='right'>&nbsp;</td>
+	<td align='left'>&nbsp;</td>
+	</tr>
+	</table>
+	</body>
+	</html>
+    ";
+    
+    para = "prueba56@mailinator.com";
+
+    if(mail($para, 'Formulario Franquicias', $message, $headers)) echo'<body><link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow|PT+Sans:400,700&display=swap" rel="stylesheet"><div><div><img src="https://www.yellowpath.mx/proyectos/crepe-corner/assets/images/logo/logo-crepe_corner-color.png" alt="Logo Medestore"></div><h1>Mensaje Enviado</h1><h4>Gracias por ponerte en contacto con nosotros.<br> En breve uno de nuestros especialistas te contactará</h4></div><style>body{margin: 0;padding: 0;background-color: #fff;background-size: cover;height: 100vh;width: 100vw;}body>div {position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);display: block;text-align: center;width: 85%;}img{width: 25rem;display: inline-block;margin: 0 0 5rem;}h1{font-family: PT Sans Narrow, sans-serif;font-size: 50px;font-weight: 700;text-transform: uppercase;display: inline-block;color: #000;margin: 0 0 1.5rem;}h4{font-size: 32px;font-family: PT Sans, sans-serif;font-weight: 400;margin: 0;}</style></body>';
+    else echo'<body><link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow|PT+Sans:400,700&display=swap" rel="stylesheet"><div><div><img src="https://www.yellowpath.mx/proyectos/crepe-corner/assets/images/logo/logo-crepe_corner-color.png" alt="Logo Medestore"></div><h1>Ocurrió un problema</h1><h4>Por favor, vuelve a intentar mandar tu mensaje.</h4></div><style>body{margin: 0;padding: 0;background-color: #fff;background-size: cover;height: 100vh;width: 100vw;}body>div {position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);display: block;text-align: center;width: 85%;}img{width: 25rem;display: inline-block;margin: 0 0 5rem;}h1{font-family: PT Sans Narrow, sans-serif;font-size: 50px;font-weight: 700;text-transform: uppercase;display: inline-block;color: #000;margin: 0 0 1.5rem;}h4{font-size: 32px;font-family: PT Sans, sans-serif;font-weight: 400;margin: 0;}</style></body>';
+?>
